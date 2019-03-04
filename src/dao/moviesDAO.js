67c1c3cdff318fe293c196df585654e0abcfaf0f
Aleksandar Ristic <storiaca@gmail.com>
2019-03-04 09:51:35 +0100

    Ticket: Handling errors

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -326,12 +326,18 @@ export default class MoviesDAO {
 
       Handle the error that occurs when an invalid ID is passed to this method.
       When this specific error is thrown, the method should return `null`.
-      */
+      */ 
+      if (
+        e.toString()
+          .startsWith(
+            "Error: Argument passed in must be a single String of 12 bytes or a string of 24 hex characters",)
+      ) {
+        return null
+      }
 
-      // TODO Ticket: Error Handling
-      // Catch the InvalidId error by string matching, and then handle it.
       console.error(`Something went wrong in getMovieByID: ${e}`)
       throw e
+     
     }
   }
 }
